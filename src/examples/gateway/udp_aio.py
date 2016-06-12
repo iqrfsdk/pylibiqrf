8f49e4303fe33c36df4162c346d230196d7b9e80
fr0stbite <frzerostbite@gmail.com>
2016-06-12 15:49:32 +0200

    Separate API components from the example code.

--- src/examples/gateway/udp_aio.py
+++ src/examples/gateway/udp_aio.py
@@ -24,23 +24,7 @@ import asyncio
 import time
 
 from collections import deque
-
-class DpaMessage:
-
-    # TODO: Add methods for checking messages, calculating checksums, etc.
-
-    def __init__(self, data):
-        self.data = data
-
-    def __str__(self):
-        return "DpaMessage([{}])".format(self.data)
-
-    def encode(self):
-        return bytes(self.data)
-
-    @staticmethod
-    def decode(data):
-        return DpaMessage(list(data))
+from pylibiqrf.dpa import DpaMessage
 
 class AsyncUdpHandler:
 
---
+++ src/pylibiqrf/dpa.py
@@ -0,0 +1,16 @@
+class DpaMessage:
+
+    # TODO: Add methods for checking messages, calculating checksums, etc.
+
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return "DpaMessage([{}])".format(self.data)
+
+    def encode(self):
+        return bytes(self.data)
+
+    @staticmethod
+    def decode(data):
+        return DpaMessage(list(data))
