e237ab0dd3ef518c97b28f1f0fcc5ef13bed6b13
fr0stbite <frzerostbite@gmail.com>
2016-07-05 19:40:44 +0200

    Make the 'test.py' script suitable for CI runners.

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 import unittest
+import sys
 
-TEST_DIRECTORIES = ["test/cdc"]
+TEST_DIRECTORIES = [
+    "test/cdc"
+]
 
 def main():
+
+    exit_code = 0
+
     for directory in TEST_DIRECTORIES:
         tests = unittest.TestLoader().discover(directory, pattern="*_test.py")
-        print(unittest.TextTestRunner().run(tests))
+        result = unittest.TextTestRunner().run(tests)
+
+        if not result.wasSuccessful():
+            exit_code = 1
+
+    sys.exit(exit_code)
 
 if __name__ == "__main__":
     main()
