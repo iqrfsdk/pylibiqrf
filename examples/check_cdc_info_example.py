b68af67f919ef251e4333260bfdddcda2b94fb9c
fr0stbite <frzerostbite@gmail.com>
2016-07-14 14:52:56 +0200

    Add first CDC example.

---
+++ examples/check_cdc_info_example.py
@@ -0,0 +1,35 @@
+import argparse
+
+from iqrf import cdc
+
+ARGS = argparse.ArgumentParser(description="IQRF USB CDC communication example.")
+ARGS.add_argument("-p", "--port", action="store", dest="port", required=True, type=str, help="The port name to connect to.")
+
+def main():
+    args = ARGS.parse_args()
+    port = args.port
+    device = None
+
+    try:
+        device = cdc.open(port)
+        test = device.send(cdc.TestRequest(), timeout=5)
+
+        if test.status == cdc.CdcStatus.OK:
+            print("Requesting device info...")
+
+            info = device.send(cdc.InfoRequest())
+
+            print("Communicating with {} v{}.".format(info.type, info.version))
+            print("The internal ID of this device is: {}.".format(info.id))
+        else:
+            print("Test request failed!")
+
+    except Exception as error:
+        print("An error occured:", type(error), error)
+        raise error
+    finally:
+        if device is not None:
+            device.close()
+
+if __name__ == "__main__":
+    main()
