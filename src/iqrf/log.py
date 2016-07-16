ffc7408138dafa7a2ae6db6b30524dc994a61e87
fr0stbite <frzerostbite@gmail.com>
2016-07-16 20:24:53 +0200

    Add documentation header to the 'iqrf.log' module.

--- src/iqrf/log.py
+++ src/iqrf/log.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Log
+===
+
+This module provides simple logging functionalities.
+
+:copyright: (c) 2012 by Tomáš Rottenberg.
+:license:  Apache 2, see license.txt for more details.
+
+"""
+
 import logging
 
 logger = logging.getLogger(__package__)
