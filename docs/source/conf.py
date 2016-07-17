606c02b606c5b09cf4de128f29da9ae711014cf8
fr0stbite <frzerostbite@gmail.com>
2016-07-17 22:01:56 +0200

    Remove source path inclusion from the documentation generator config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,6 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../src'))
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
